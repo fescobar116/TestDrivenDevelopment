12faf61f16f30b181355c171d31591dbe6f094ab
fescobar116 <docente.fescobar@unac.edu.co>
2026-08-18 19:32:39 -0500

    tdd tests

--- src/main/java/edu/unac/RomanNumberConvertor.java
+++ src/main/java/edu/unac/RomanNumberConvertor.java
@@ -1,7 +1,29 @@
 package edu.unac;
 
+import java.util.List;
+
 public class RomanNumberConvertor {
-    public String convert(int number){
-        return "I".repeat(number);
+
+    private record RomanSymbol(int value, String symbol){};
+    private static final List<RomanSymbol> SYMBOLS = List.of(
+            new RomanSymbol(90, "XC"),
+            new RomanSymbol(50, "L"),
+            new RomanSymbol(40, "XL"),
+            new RomanSymbol(10, "X"),
+            new RomanSymbol(9, "IX"),
+            new RomanSymbol(5, "V"),
+            new RomanSymbol(4, "IV"),
+            new RomanSymbol(1, "I")
+    );
+
+    public String convert(int number) {
+        String result = "";
+        for (RomanSymbol romanSymbol : SYMBOLS){
+            while (number >= romanSymbol.value()){
+                result += romanSymbol.symbol();
+                number -= romanSymbol.value();
+            }
+        }
+        return result;
     }
 }
--- src/test/java/edu/unac/RomanNumberConvertorTest.java
+++ src/test/java/edu/unac/RomanNumberConvertorTest.java
@@ -33,4 +33,121 @@ class RomanNumberConvertorTest {
         assertEquals("IV", convertor.convert(4));
     }
 
+    @Test
+    void shouldConvertToV(){
+        RomanNumberConvertor convertor = new RomanNumberConvertor();
+
+        assertEquals("V", convertor.convert(5));
+    }
+    @Test
+    void shouldConvertToVI(){
+        RomanNumberConvertor convertor = new RomanNumberConvertor();
+
+        assertEquals("VI", convertor.convert(6));
+    }
+
+    @Test
+    void shouldConvertToVII(){
+        RomanNumberConvertor convertor = new RomanNumberConvertor();
+
+        assertEquals("VII", convertor.convert(7));
+    }
+
+    @Test
+    void shouldConvertToIX(){
+        RomanNumberConvertor convertor = new RomanNumberConvertor();
+
+        assertEquals("IX", convertor.convert(9));
+    }
+
+    @Test
+    void shouldConvertToX(){
+        RomanNumberConvertor convertor = new RomanNumberConvertor();
+
+        assertEquals("X", convertor.convert(10));
+    }
+
+    @Test
+    void shouldConvertToXI(){
+        RomanNumberConvertor convertor = new RomanNumberConvertor();
+
+        assertEquals("XI", convertor.convert(11));
+    }
+
+    @Test
+    void shouldConvertToXIV(){
+        RomanNumberConvertor convertor = new RomanNumberConvertor();
+
+        assertEquals("XIV", convertor.convert(14));
+    }
+
+    @Test
+    void shouldConvertToXV(){
+        RomanNumberConvertor convertor = new RomanNumberConvertor();
+
+        assertEquals("XV", convertor.convert(15));
+    }
+
+    @Test
+    void shouldConvertToXVI(){
+        RomanNumberConvertor convertor = new RomanNumberConvertor();
+
+        assertEquals("XVI", convertor.convert(16));
+    }
+
+    @Test
+    void shouldConvertToXVIII(){
+        RomanNumberConvertor convertor = new RomanNumberConvertor();
+
+        assertEquals("XVIII", convertor.convert(18));
+    }
+
+    @Test
+    void shouldConvertToXIX(){
+        RomanNumberConvertor convertor = new RomanNumberConvertor();
+
+        assertEquals("XIX", convertor.convert(19));
+    }
+
+    @Test
+    void shouldConvertToXX(){
+        RomanNumberConvertor convertor = new RomanNumberConvertor();
+
+        assertEquals("XX", convertor.convert(20));
+    }
+
+    @Test
+    void shouldConvertToXXI(){
+        RomanNumberConvertor convertor = new RomanNumberConvertor();
+
+        assertEquals("XXI", convertor.convert(21));
+    }
+
+    @Test
+    void shouldConvertToXL(){
+        RomanNumberConvertor convertor = new RomanNumberConvertor();
+
+        assertEquals("XL", convertor.convert(40));
+    }
+
+    @Test
+    void shouldConvertToL(){
+        RomanNumberConvertor convertor = new RomanNumberConvertor();
+
+        assertEquals("L", convertor.convert(50));
+    }
+
+    @Test
+    void shouldConvertToLXXIX(){
+        RomanNumberConvertor convertor = new RomanNumberConvertor();
+
+        assertEquals("LXXIX", convertor.convert(79));
+    }
+
+    @Test
+    void shouldConvertToXC(){
+        RomanNumberConvertor convertor = new RomanNumberConvertor();
+
+        assertEquals("XC", convertor.convert(90));
+    }
 }
